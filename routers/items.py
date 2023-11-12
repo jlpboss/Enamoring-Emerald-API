50d6baf876d979829226f67d4b580929215c5a9f
Jett Pennington <jettpennington@gmail.com>
2023-11-12 23:03:20 +0000

    testing

--- routers/items.py
+++ routers/items.py
@@ -1,9 +1,26 @@
 from typing import List
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, FastAPI
 import schemas
 import crud
 from database import SessionLocal
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+
+SECRET_KEY = "test"
+ALGORITHM = "HS256"
+
+def create_jwt_token(data: dict):
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_jwt_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 router = APIRouter(
     prefix="/menu-items"
@@ -20,3 +37,23 @@ def get_db():
 def get_items(db: Session = Depends(get_db)):
     items = crud.get_menu_items(db)
     return items
+
+@router.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    # Authenticate user (you need to implement this part)
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    # Create JWT token
+    token_data = {"sub": user.username}
+    token = create_jwt_token(token_data)
+
+    return {"access_token": token, "token_type": "bearer"}
+
+@router.get("/protected")
+def protected_route(token: str = Depends(oauth2_scheme)):
+    # Decode and verify JWT token
+    token_data = decode_jwt_token(token)
+    return {"message": "You are in a protected route", "token_data": token_data}
